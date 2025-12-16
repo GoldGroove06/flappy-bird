1b0f4ca70007b5f99b0298d45e0aadb208865640
GoldGroove06 <73437174+GoldGroove06@users.noreply.github.com>
2025-12-16 19:41:12 +0530

    added hud

--- bird.gd
+++ bird.gd
@@ -14,4 +14,5 @@ func _process(delta):
 
 
 func _on_body_entered(body: Node):
+	hit.emit()
 	print("hit")
--- main.gd
+++ main.gd
@@ -2,9 +2,25 @@ extends Node
 @export var pipe_scene: PackedScene
 
 func _ready():
-	$PipeTimer.start()
-
+	$Bird.hide()
+	
 func _on_pipe_timer_timeout():
 	var pipe = pipe_scene.instantiate()
 	pipe.position = Vector2(480,randf_range(200,400))
 	add_child(pipe)
+
+func game_start():
+	var score = 0
+	$Bird.show()
+	get_tree().call_group("pipes", "queue_free")
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$PipeTimer.start()
+	
+
+func _on_bird_hit():
+	$PipeTimer.stop()
+	$HUD.show_game_over()
+
+func _on_hud_start_game():
+	game_start()
